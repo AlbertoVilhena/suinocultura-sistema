42b266b88e07da86abc0e16ce55ad67f04136f68
AlbertoVilhena <albertovilhena@gmail.com>
2025-03-27 12:25:06 -0300

    Delete app directory

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.routers import lotes, pesagens, estoque, ia
-
-app = FastAPI()
-
-app.include_router(lotes.router)
-app.include_router(pesagens.router)
-app.include_router(estoque.router)
-app.include_router(ia.router)
--- app/routers/ia.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/ia", tags=["Inteligência Artificial"])
-
-@router.get("/")
-def obter_recomendacoes():
-    return {"msg": "Recomendações de IA"}
