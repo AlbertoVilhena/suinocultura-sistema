621918e914aa594a594252c1c4d8090c98ad2fab
AlbertoVilhena <albertovilhena@gmail.com>
2025-08-02 19:11:30 -0300

    Update usuario.py

--- models/usuario.py
+++ models/usuario.py
@@ -5,4 +5,5 @@ class Usuario(Base):
     __tablename__ = "usuarios"
 
     id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    senha = Column(String, nullable=False)
